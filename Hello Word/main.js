ae2a6e31049bf6d8770d9784cf30ac2b7ad55999
Anielly28 <166028943+Anielly28@users.noreply.github.com>
2024-05-09 13:01:25 -0300

    Add files via upload

---
+++ Hello Word/main.js
@@ -0,0 +1,11 @@
+let nomeUsuario = prompt ("Qual o seu nome?");
+let elemento = document.querySelector("#nome-usuario")
+
+let nomeUsuario = "";
+
+
+While (nomeusuario == "") 
+{
+   nomeusuario = propt("Qual o seu nome?")
+}
+
